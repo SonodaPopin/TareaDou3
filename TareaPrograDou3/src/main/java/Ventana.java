f228e586f2e21ddfde303487e2ad83758e5e26c8
SonodaPopin <joahernandez2022@udec.cl>
2023-11-02 20:47:47 -0300

    Prueba interfaz grafica

--- TareaPrograDou3/src/main/java/Ventana.java
+++ TareaPrograDou3/src/main/java/Ventana.java
@@ -6,8 +6,17 @@ public class Ventana extends JFrame {
         super();
         this.setLayout(new BorderLayout());
         this.setTitle("DOU");
+
+        this.add(new JButton("norte"),BorderLayout.NORTH);
+        //this.add(new JButton("este"),BorderLayout.EAST);
+        //this.add(new JButton("oeste"),BorderLayout.WEST);
+        this.add(new JButton("centro"),BorderLayout.CENTER);
+        this.add(new JButton("sur"),BorderLayout.SOUTH);
+        this.add(new PanelComprador(),BorderLayout.WEST);
+        this.add(new PanelExpendedor(),BorderLayout.EAST);
+
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(1000,800);
+        this.setSize(800,600);
         this.setVisible(true);
     }
 }
