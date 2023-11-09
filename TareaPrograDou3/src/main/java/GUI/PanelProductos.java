7a99af224178b96a80e00426fa5681c12de8f8df
SonodaPopin <joahernandez2022@udec.cl>
2023-11-09 19:14:19 -0300

    Nombres de productos en los rectangulos

--- TareaPrograDou3/src/main/java/GUI/PanelProductos.java
+++ TareaPrograDou3/src/main/java/GUI/PanelProductos.java
@@ -15,17 +15,27 @@ public class PanelProductos extends JPanel {
 
         g.setColor(Color.green);
         g.fillRect(30, 30, 100, 200);
+        g.setColor(Color.BLACK);
+        g.drawString("Sprite", 55, 60);
 
         g.setColor(Color.cyan);
         g.fillRect(150, 30, 100, 200);
+        g.setColor(Color.BLACK);
+        g.drawString("CocaCola", 175, 60);
 
         g.setColor(Color.ORANGE);
         g.fillRect(270, 30, 100, 200);
+        g.setColor(Color.BLACK);
+        g.drawString("Fanta", 295, 60);
 
         g.setColor(Color.PINK);
         g.fillRect(210, 250, 100, 200);
+        g.setColor(Color.BLACK);
+        g.drawString("Super8", 235, 280);
 
         g.setColor(Color.WHITE);
         g.fillRect(90, 250, 100, 200);
+        g.setColor(Color.BLACK);
+        g.drawString("Snicker", 115, 280);
     }
 }
